78cc4e799ce33e9cc8e35649f691490550d37e67
devsidney <silvasidney721@gmail.com>
2022-10-22 08:54:42 +0200

    Creación módulos + otras cosas.

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HelloWorld } from './primerComponente/primer.component' // Importamos l
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponentComponent } from './about-component/about-component.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorld,
-    AboutComponentComponent // Usamos el componente que acabamos de importar.
+    AboutComponentComponent,
+    UserComponent // Usamos el componente que acabamos de importar.
   ],
   imports: [
     BrowserModule,
